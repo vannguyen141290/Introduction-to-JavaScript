009d5722e08e61a7c67115fc2115d4f02addb298
Sanoo Singh <vannguyen141290@gmail.com>
2021-07-13 14:24:52 -0700

    Javascript-Intro day 1 Project -Completed

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ Do the following:
 
    HINT: no function required
 */
+let votingAge = 15;
+if (votingAge >= 18){
+  console.log(true);
+}
+else {
+  console.log(false);
+}
 
 
 
@@ -32,9 +39,12 @@ Do the following:
 
    HINT: no function required
 */
-
-
-
+let firstVar = 'van';
+let secondVar = 'nguyen';
+if (firstVar !== secondVar){
+  firstVar = secondVar;
+  console.log(firstVar);
+};
 
 
 /*
@@ -47,7 +57,8 @@ Do the following:
 
    HINT: look up the Number method
 */
-
+let string ='1999';
+console.log(parseInt(string));
 
 
 
@@ -60,9 +71,11 @@ Do the following:
    3. Multiply a and b and return the answer
 */
 
-function multiply(/*make some changesadd your code here*/){
-  /*add your code here*/
+function multiply(a, b){
+  return a * b;
 }
+
+console.log(multiply(2 , 3));
 
 
 
@@ -76,10 +89,10 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
+function dogYears(humanYears){
+  return humanYears * 7;
 }
-
+dogYears(5);
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -109,9 +122,34 @@ Puppies less than 1 year
 NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.44999999999999996
 */  
 
-function hungryDog(/*add your code here*/){
-  /*add your code here*/
+function hungryDog(weight, age){
+  if (age >= 1){
+    if (weight <= 5){
+      return weight * 0.05;
+    }
+    else if (weight >=6 && weight <= 10){
+      return weight * 0.04;
+    }
+    else if (weight >= 11 && weight <=15){
+      return weight * 0.03;
+    } else {
+      return weight * 0.02;
+    }
+  }
+  else if (age < 1){
+    if (age >= 2/12 && age <= 4/12){
+      return weight * 0.1;
+    } 
+    else if (age > 4/12 && age < 0.582) {
+      return weight * 0.05;
+    } 
+    else if (age >= 0.582 && age < 1) {
+      return weight * 0.04;
+    }
+  }
 }
+
+console.log(hungryDog(15, 1));
 
 
 
@@ -173,9 +211,12 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
+function miles(km){
+  return km * 0.621371
   /*add your code here*/
 }
+
+console.log(miles(2));
 
 
 
@@ -187,10 +228,12 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
+function feet(cm){
+  return cm / 30.48;
   /*add your code here*/
 }
 
+console.log(feet(50));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -205,11 +248,11 @@ Using the annoyingSong function below do the following:
 
 function annoyingSong(number){
   for (let i=number; i>0; i--){
-    return `${i} bottles of soda on the wall, ${i} of soda, take one down pass it around ${i-1} bottles of soda on the wall`;
+    return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i-1} bottles of soda on the wall`;
   }
 }
 
-console.log('task 6', annoyingSong(99));
+console.log(annoyingSong(99));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -227,9 +270,25 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(score){
+  if (score >=90 && score <=100){
+    return 'you got an A';
+  }
+  else if (score >=80 && score <=89){
+    return 'you got a B';
+  }
+  else if (score >=70 && score <=79){
+    return 'you got a C';
+  }
+  else if (score >=60 && score <=69){
+    return 'you got a D';
+  }
+  else if (score < 60){
+    return 'you got an F';
+  }
 }
+
+console.log(grade(100));
 
 
 
